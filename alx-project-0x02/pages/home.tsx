828e332fc3044ecb0c3cf8c9456b92c50adeb033
AshenafiDegefe <ashenafidegefe8@gmail.com>
2025-10-22 14:44:44 +0300

    Create a Reusable Card Component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,12 @@
 import Header from "@/components/layout/Header";
+import Card from "@/components/common/Card";
+
 export default function Home() {
     return (
         <div>
             <Header />
             <h1>Home Page</h1>
+            <Card title="Welcome" content="This is the home page card content." />
         </div>
     )
 }
